7137d5000deaaeda8751ab04df54284351c475fd
dcode-physics <dylantrentpeddie@outlook.com>
2026-01-16 11:59:16 +0200

    kepler_orbits: add first-order Kepler ODE system

--- level-1-foundations/kepler_orbits/src/dynamics.py
+++ level-1-foundations/kepler_orbits/src/dynamics.py
@@ -22,3 +22,23 @@ def acceleration(r: np.ndarray, params: ProblemParams) -> np.ndarray:
     r2 = r[0]**2 + r[1]**2
     rmag = np.sqrt(r2)
     return -(params.mu * r) / (rmag**3)
+
+
+def kepler_rhs(t: float, y: np.ndarray, params: ProblemParams) -> np.ndarray:
+    '''
+    First-order ODE system for planar Kepler problem
+
+    state vector y = [x, y, v_x, v_y]
+    '''
+
+    r = y[0:2]
+    v = y[2:4]
+
+    a = acceleration(r, params)
+
+    return np.array([
+        v[0],
+        v[1],
+        a[0],
+        a[1],
+    ], dtype = float)
